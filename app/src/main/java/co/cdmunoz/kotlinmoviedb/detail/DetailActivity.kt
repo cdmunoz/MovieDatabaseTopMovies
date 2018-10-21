2706304def3935cd402d6306e1c2eef308b8203f
carlosmunoz <carlosmunoz@thepublicgroup.com>
2018-10-21 14:12:40 -0500

    Create new activity for details

---
+++ app/src/main/java/co/cdmunoz/kotlinmoviedb/detail/DetailActivity.kt
@@ -0,0 +1,13 @@
+package co.cdmunoz.kotlinmoviedb.detail
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import co.cdmunoz.kotlinmoviedb.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
